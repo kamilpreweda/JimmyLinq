d238f05073d712ad38b8b76ca856d1d2aaed7c02
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-16 18:09:59 +0200

    Added Unit test for 'ComicAnalyzer' Reviews generating method

--- JimmyLinqUnitTests/ComicAnalyzerTests.cs
+++ JimmyLinqUnitTests/ComicAnalyzerTests.cs
@@ -31,5 +31,28 @@ namespace JimmyLinqUnitTests
             Assert.AreEqual("Numer 2", groups.First().First().Name);
 
         }
+
+        [TestMethod]
+        public void ComicAnalyzer_Should_Generate_A_List_Of_Reviews()
+        {
+            var testReviews = new[]
+            {
+                new Review() {Issue = 1, Critic = Critics.MuddyCritic, Score = 14.5},
+                new Review() {Issue = 1, Critic = Critics.RottenTornadoes, Score = 59.93},
+                new Review() {Issue = 2, Critic = Critics.MuddyCritic, Score = 40.3},
+                new Review() {Issue = 2, Critic = Critics.RottenTornadoes, Score = 95.11},
+            };
+
+            var expectedResults = new[]
+            {
+                "MuddyCritic oceni³ nr 1 'Numer 1' na 14,50.",
+                "RottenTornadoes oceni³ nr 1 'Numer 1' na 59,93.",
+                "MuddyCritic oceni³ nr 2 'Numer 2' na 40,30.",
+                "RottenTornadoes oceni³ nr 2 'Numer 2' na 95,11.",
+            };
+
+            var actualResults = ComicAnalyzer.GetReviews(testComics, testReviews).ToList();
+            CollectionAssert.AreEqual(expectedResults, actualResults);
+        }
     }
 }
